adcafd717a680b769614bc760271140ba4641caa
Nicholas Rodine <Nicholas.Rodine@cinemassive.com>
2015-03-11 13:12:27 -0400

    Extracted out a few methods.

--- PokerHands/HandRanker.cs
+++ PokerHands/HandRanker.cs
@@ -5,6 +5,13 @@ namespace PokerHands
 {
     public class HandRanker
     {
+        private List<CardValueGroup> _hand1CardValueGroups;
+        private List<CardValueGroup> _hand2CardValueGroups;
+        private int _oVl;
+        private bool _hand1HasFiveCardGroups;
+        private int _tVl;
+        private bool _hand2HasFiveCardGroups;
+
         private class CardValueGroup
         {
             public CardValue Value { get; set; }
@@ -13,100 +20,32 @@ namespace PokerHands
 
         public int RankHands(IList<Card> hand1, IList<Card> hand2)
         {
-            var hand1CardValueGroups = GetCardValueGroups(hand1);
+            GetCardHandGroups(hand1, hand2);
 
-            var hand2CardValueGroups = GetCardValueGroups(hand2);
-
-            var oVl = hand1CardValueGroups.OrderBy(i => (int)i.Value).Select(i => (int)i.Value).FirstOrDefault();
-
-            var oCon = hand1CardValueGroups.Count == 5;
-
-            if (hand1CardValueGroups.Count == 5)
-            {
-                if (hand1CardValueGroups.Any(i => i.Value == CardValue.Ace))
-                {
-                    for (var i = 0; i < 4; i++)
-                    {
-                        if (hand1CardValueGroups.Any(v => (int)v.Value == (i))) ;
-                        else
-                        {
-                            oCon = false;
-
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    for (var i = 0; i < hand1CardValueGroups.Count; i++)
-                    {
-                        if (hand1CardValueGroups.Any(v => (int)v.Value == (oVl))) oVl = oVl + 1;
-                        else
-                        {
-                            oCon = false;
-
-                            break;
-                        }
-                    }
-                }
-            }
-
-            var tVl = hand2CardValueGroups.OrderBy(i => (int)i.Value).Select(i => (int)i.Value).FirstOrDefault();
-
-            var tCon = hand2CardValueGroups.Count == 5;
-
-            if (hand2CardValueGroups.All(i => i.Count > 0 && i.Count < 2))
-            {
-                if (hand2CardValueGroups.Any(i => i.Value == CardValue.Ace))
-                {
-                    for (var i = 0; i < 4; i++)
-                    {
-                        if (hand2CardValueGroups.Any(v => (int)v.Value == (i))) ;
-                        else
-                        {
-                            tCon = false;
-
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    foreach (var value in hand2CardValueGroups)
-                    {
-                        if (hand2CardValueGroups.Any(v => (int)v.Value == (tVl))) tVl = tVl + 1;
-                        else
-                        {
-                            tCon = false;
-
-                            break;
-                        }
-                    }
-                }
-            }
+            ProcessBothHands();
 
             var oF = hand1.All(i => i.Suit == hand1.Select(j => j.Suit).FirstOrDefault());
             var tF = hand2.All(i => i.Suit == hand2.Select(j => j.Suit).FirstOrDefault());
 
-            if ((oF && oCon) || (tF && tCon))
+            if ((oF && _hand1HasFiveCardGroups) || (tF && _hand2HasFiveCardGroups))
             {
-                if (!(oF && oCon)) return 2;
+                if (!(oF && _hand1HasFiveCardGroups)) return 2;
 
-                if (!(tF && tCon)) return 1;
+                if (!(tF && _hand2HasFiveCardGroups)) return 1;
 
-                var o_m_v_12 = hand1CardValueGroups.OrderByDescending(i => (int)i.Value).Select(i => (int)i.Value).FirstOrDefault();
-                var tMVM = hand2CardValueGroups.OrderByDescending(i => (int)i.Value).Select(i => (int)i.Value).FirstOrDefault();
+                var o_m_v_12 = _hand1CardValueGroups.OrderByDescending(i => (int)i.Value).Select(i => (int)i.Value).FirstOrDefault();
+                var tMVM = _hand2CardValueGroups.OrderByDescending(i => (int)i.Value).Select(i => (int)i.Value).FirstOrDefault();
 
                 if (o_m_v_12 == 12)
                 {
-                    var lowCard = hand1CardValueGroups.OrderBy(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
+                    var lowCard = _hand1CardValueGroups.OrderBy(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
 
                     if (lowCard == 0) o_m_v_12 = 3;
                 }
 
                 if (tMVM == 12)
                 {
-                    var lowCard = hand2CardValueGroups.OrderBy(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
+                    var lowCard = _hand2CardValueGroups.OrderBy(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
 
                     if (lowCard == 0) tMVM = 3;
                 }
@@ -116,25 +55,25 @@ namespace PokerHands
                 if (o_m_v_12 < tMVM) return 2;
             }
 
-            if (hand1CardValueGroups.Any(i => i.Count == 4) || hand2CardValueGroups.Any(i => i.Count == 4))
+            if (_hand1CardValueGroups.Any(i => i.Count == 4) || _hand2CardValueGroups.Any(i => i.Count == 4))
             {
-                if (hand1CardValueGroups.Any(i => i.Count == 4) && hand2CardValueGroups.All(i => i.Count != 4)) return 1;
+                if (_hand1CardValueGroups.Any(i => i.Count == 4) && _hand2CardValueGroups.All(i => i.Count != 4)) return 1;
 
-                if (hand2CardValueGroups.Any(i => i.Count == 4) && hand1CardValueGroups.All(i => i.Count != 4)) return 2;
+                if (_hand2CardValueGroups.Any(i => i.Count == 4) && _hand1CardValueGroups.All(i => i.Count != 4)) return 2;
 
-                var oFKIOD = hand1CardValueGroups.Where(i => i.Count == 4).Select(i => (int)i.Value).FirstOrDefault();
-                var T_why_Not = hand2CardValueGroups.Where(i => i.Count == 4).Select(i => (int)i.Value).FirstOrDefault();
+                var oFKIOD = _hand1CardValueGroups.Where(i => i.Count == 4).Select(i => (int)i.Value).FirstOrDefault();
+                var T_why_Not = _hand2CardValueGroups.Where(i => i.Count == 4).Select(i => (int)i.Value).FirstOrDefault();
 
                 if (oFKIOD > T_why_Not) return 1;
 
                 if (T_why_Not > oFKIOD) return 2;
             }
 
-            if ((hand1CardValueGroups.Any(i => i.Count == 3) && hand1CardValueGroups.Any(i => i.Count == 2)) || (hand2CardValueGroups.Any(i => i.Count == 3) && hand2CardValueGroups.Any(i => i.Count == 2)))
+            if ((_hand1CardValueGroups.Any(i => i.Count == 3) && _hand1CardValueGroups.Any(i => i.Count == 2)) || (_hand2CardValueGroups.Any(i => i.Count == 3) && _hand2CardValueGroups.Any(i => i.Count == 2)))
             {
-                if ((hand1CardValueGroups.Any(i => i.Count == 3) && hand1CardValueGroups.Any(i => i.Count == 2)) && !(hand2CardValueGroups.Any(i => i.Count == 3) && hand2CardValueGroups.Any(i => i.Count == 2))) return 1;
+                if ((_hand1CardValueGroups.Any(i => i.Count == 3) && _hand1CardValueGroups.Any(i => i.Count == 2)) && !(_hand2CardValueGroups.Any(i => i.Count == 3) && _hand2CardValueGroups.Any(i => i.Count == 2))) return 1;
 
-                if (!(hand1CardValueGroups.Any(i => i.Count == 3) && hand1CardValueGroups.Any(i => i.Count == 2))) return 2;
+                if (!(_hand1CardValueGroups.Any(i => i.Count == 3) && _hand1CardValueGroups.Any(i => i.Count == 2))) return 2;
             }
 
             if (oF || tF)
@@ -144,25 +83,25 @@ namespace PokerHands
                 if (!oF) return 2;
             }
 
-            if (oCon || tCon)
+            if (_hand1HasFiveCardGroups || _hand2HasFiveCardGroups)
             {
-                if (oCon && !tCon) return 1;
+                if (_hand1HasFiveCardGroups && !_hand2HasFiveCardGroups) return 1;
 
-                if (!oCon) return 2;
+                if (!_hand1HasFiveCardGroups) return 2;
 
-                var o9 = hand1CardValueGroups.OrderByDescending(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
-                var T2 = hand2CardValueGroups.OrderByDescending(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
+                var o9 = _hand1CardValueGroups.OrderByDescending(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
+                var T2 = _hand2CardValueGroups.OrderByDescending(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
 
                 if (o9 == 12)
                 {
-                    var lowCard = hand1CardValueGroups.OrderBy(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
+                    var lowCard = _hand1CardValueGroups.OrderBy(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
 
                     if (lowCard == 0) o9 = 3;
                 }
 
                 if (T2 == 12)
                 {
-                    var lowCard = hand2CardValueGroups.OrderBy(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
+                    var lowCard = _hand2CardValueGroups.OrderBy(i => i.Value).Select(i => (int)i.Value).FirstOrDefault();
 
                     if (lowCard == 0) T2 = 3;
                 }
@@ -174,41 +113,41 @@ namespace PokerHands
                 return -1;
             }
 
-            if (hand1CardValueGroups.Any(i => i.Count == 3) || hand2CardValueGroups.Any(i => i.Count == 3))
+            if (_hand1CardValueGroups.Any(i => i.Count == 3) || _hand2CardValueGroups.Any(i => i.Count == 3))
             {
-                var o90 = hand1CardValueGroups.Where(i => i.Count == 3).Select(i => i.Value).FirstOrDefault();
-                var T3Fv = hand2CardValueGroups.Where(i => i.Count == 3).Select(i => i.Value).FirstOrDefault();
+                var o90 = _hand1CardValueGroups.Where(i => i.Count == 3).Select(i => i.Value).FirstOrDefault();
+                var T3Fv = _hand2CardValueGroups.Where(i => i.Count == 3).Select(i => i.Value).FirstOrDefault();
 
                 if (o90 > T3Fv) return 1;
 
                 if (T3Fv > o90) return 2;
             }
 
-            if (hand1CardValueGroups.Count(i => i.Count == 2) == 2 || hand2CardValueGroups.Count(i => i.Count == 2) == 2)
+            if (_hand1CardValueGroups.Count(i => i.Count == 2) == 2 || _hand2CardValueGroups.Count(i => i.Count == 2) == 2)
             {
-                if (hand1CardValueGroups.Count(i => i.Count == 2) == 2 && hand2CardValueGroups.Count(i => i.Count == 2) != 2) return 1;
+                if (_hand1CardValueGroups.Count(i => i.Count == 2) == 2 && _hand2CardValueGroups.Count(i => i.Count == 2) != 2) return 1;
 
-                if (hand2CardValueGroups.Count(i => i.Count == 2) == 2 && hand1CardValueGroups.Count(i => i.Count == 2) != 2) return 2;
+                if (_hand2CardValueGroups.Count(i => i.Count == 2) == 2 && _hand1CardValueGroups.Count(i => i.Count == 2) != 2) return 2;
 
-                var o78658 = hand1CardValueGroups.Where(i => i.Count == 2).OrderByDescending(i => i.Value).Select(i => i.Value).FirstOrDefault();
-                var sIsAfterT = hand2CardValueGroups.Where(i => i.Count == 2).OrderByDescending(i => i.Value).Select(i => i.Value).FirstOrDefault();
+                var o78658 = _hand1CardValueGroups.Where(i => i.Count == 2).OrderByDescending(i => i.Value).Select(i => i.Value).FirstOrDefault();
+                var sIsAfterT = _hand2CardValueGroups.Where(i => i.Count == 2).OrderByDescending(i => i.Value).Select(i => i.Value).FirstOrDefault();
 
                 if (o78658 > sIsAfterT) return 1;
 
                 if (o78658 < sIsAfterT) return 2;
 
-                hand1CardValueGroups = hand1CardValueGroups.Where(i => i.Value != o78658).ToList();
-                hand2CardValueGroups = hand2CardValueGroups.Where(i => i.Value != sIsAfterT).ToList();
+                _hand1CardValueGroups = _hand1CardValueGroups.Where(i => i.Value != o78658).ToList();
+                _hand2CardValueGroups = _hand2CardValueGroups.Where(i => i.Value != sIsAfterT).ToList();
             }
 
-            if (hand1CardValueGroups.Any(i => i.Count == 2) || hand2CardValueGroups.Any(i => i.Count == 2))
+            if (_hand1CardValueGroups.Any(i => i.Count == 2) || _hand2CardValueGroups.Any(i => i.Count == 2))
             {
-                if (hand1CardValueGroups.Any(i => i.Count == 2) && hand2CardValueGroups.All(i => i.Count != 2)) return 1;
+                if (_hand1CardValueGroups.Any(i => i.Count == 2) && _hand2CardValueGroups.All(i => i.Count != 2)) return 1;
 
-                if (hand1CardValueGroups.All(i => i.Count != 2) && hand2CardValueGroups.Any(i => i.Count == 2)) return 2;
+                if (_hand1CardValueGroups.All(i => i.Count != 2) && _hand2CardValueGroups.Any(i => i.Count == 2)) return 2;
 
-                var o123894_Griffins = hand1CardValueGroups.Where(i => i.Count == 2).Select(i => i.Value).FirstOrDefault();
-                var t_24_This_IsWrong = hand2CardValueGroups.Where(i => i.Count == 2).Select(i => i.Value).FirstOrDefault();
+                var o123894_Griffins = _hand1CardValueGroups.Where(i => i.Count == 2).Select(i => i.Value).FirstOrDefault();
+                var t_24_This_IsWrong = _hand2CardValueGroups.Where(i => i.Count == 2).Select(i => i.Value).FirstOrDefault();
 
                 if (o123894_Griffins > t_24_This_IsWrong) return 1;
 
@@ -299,6 +238,84 @@ namespace PokerHands
             if (hand1.Max(i => (int)i.CardValue) < hand2.Max(i => (int)i.CardValue)) return 2;
 
             return -1;
+        }
+
+        private void ProcessBothHands()
+        {
+            _oVl = _hand1CardValueGroups.OrderBy(i => (int) i.Value).Select(i => (int) i.Value).FirstOrDefault();
+
+            _hand1HasFiveCardGroups = _hand1CardValueGroups.Count == 5;
+
+            if (_hand1CardValueGroups.Count == 5)
+            {
+                if (_hand1CardValueGroups.Any(i => i.Value == CardValue.Ace))
+                {
+                    for (var i = 0; i < 4; i++)
+                    {
+                        if (_hand1CardValueGroups.Any(v => (int) v.Value == (i))) ;
+                        else
+                        {
+                            _hand1HasFiveCardGroups = false;
+
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    for (var i = 0; i < _hand1CardValueGroups.Count; i++)
+                    {
+                        if (_hand1CardValueGroups.Any(v => (int) v.Value == (_oVl))) _oVl = _oVl + 1;
+                        else
+                        {
+                            _hand1HasFiveCardGroups = false;
+
+                            break;
+                        }
+                    }
+                }
+            }
+
+            _tVl = _hand2CardValueGroups.OrderBy(i => (int) i.Value).Select(i => (int) i.Value).FirstOrDefault();
+
+            _hand2HasFiveCardGroups = _hand2CardValueGroups.Count == 5;
+
+            if (_hand2CardValueGroups.All(i => i.Count > 0 && i.Count < 2))
+            {
+                if (_hand2CardValueGroups.Any(i => i.Value == CardValue.Ace))
+                {
+                    for (var i = 0; i < 4; i++)
+                    {
+                        if (_hand2CardValueGroups.Any(v => (int) v.Value == (i))) ;
+                        else
+                        {
+                            _hand2HasFiveCardGroups = false;
+
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var value in _hand2CardValueGroups)
+                    {
+                        if (_hand2CardValueGroups.Any(v => (int) v.Value == (_tVl))) _tVl = _tVl + 1;
+                        else
+                        {
+                            _hand2HasFiveCardGroups = false;
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GetCardHandGroups(IList<Card> hand1, IList<Card> hand2)
+        {
+            _hand1CardValueGroups = GetCardValueGroups(hand1);
+
+            _hand2CardValueGroups = GetCardValueGroups(hand2);
         }
 
         private static List<CardValueGroup> GetCardValueGroups(IList<Card> hand)
